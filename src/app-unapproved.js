8aa0448c47ea048849765a4afb7128a6b14c8fc2
Ryan Roemer <ryan.roemer@formidable.com>
2020-09-20 14:08:52 -0700

    More examples

--- src/app-unapproved.js
+++ src/app-unapproved.js
@@ -8,8 +8,11 @@ import fullpage from "fullpage.js";
 // This _should_ be OK: `(MIT OR GPL-3.0+)`
 // https://www.npmjs.com/package/scrollmagic
 import scrollmagic from "scrollmagic";
+// WTFPL
+import leftPad from "left-pad";
 
 console.log({
   fullpage,
-  scrollmagic
+  scrollmagic,
+  leftPad
 });
