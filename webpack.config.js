0c6ee761e1ed95ce5cd918613bb925dc74287dee
Ryan Roemer <ryan.roemer@formidable.com>
2020-09-20 13:46:54 -0700

    more config

--- webpack.config.js
+++ webpack.config.js
@@ -4,30 +4,55 @@ const path = require("path");
 const chalk = require("chalk");
 const LicenseCheckerWebpackPlugin = require("license-checker-webpack-plugin");
 
-const LICENSES = [
+// ----------------------------------------------------------------------------
+// License Configuration
+// ----------------------------------------------------------------------------
+// Create straight list of allowed licenses for ease of review.
+const ALLOWED_LICENSES = [
   "Apache-2.0",
+  "0BSD",
   "BSD-2-Clause",
   "BSD-3-Clause",
   "MIT"
 ];
-const LICENSE_OPTS = `(${LICENSES.join(" OR ")}`;
+// Convert to plugin options.
+// See: https://github.com/microsoft/license-checker-webpack-plugin#options
+const LICENSE_CHECKER_OPTS = {
+  // Base license permissions.
+  allow: `(${ALLOWED_LICENSES.join(" OR ")})`,
+  // Manually reviewed libraries to ignore (include comment of reason).
+  ignore: [],
+  // Manually reviewed libraries to override an SPDX license declaration (include comment of reason).
+  override: {}
+};
+
+// Create report of configuration.
 console.log(chalk`
 {cyan ## Configured Licenses:}
-${LICENSES.map((lic) =>
+${ALLOWED_LICENSES.map((lic) =>
   chalk`- ${lic}: {gray https://spdx.org/licenses/${lic}.html}`).join("\n")}
 `);
 
+// ----------------------------------------------------------------------------
+// Example Configuration
+// ----------------------------------------------------------------------------
+// App-specific example setup..
 let APPS = [
   "apollo",
   "urql"
 ];
+let FAIL_ON_UNAPPROVED = false;
 
 if (process.env.APP_ALL === "true") {
   APPS.push("unapproved");
+  FAIL_ON_UNAPPROVED = true;
 } else if (process.env.APP_UNAPPROVED === "true") {
   APPS = ["unapproved"];
 }
 
+// ----------------------------------------------------------------------------
+// Webpack Configuration
+// ----------------------------------------------------------------------------
 // Use array to simulate completely separate builds
 module.exports = APPS.map((app) => ({
   mode: "development",
@@ -42,12 +67,7 @@ module.exports = APPS.map((app) => ({
   devtool: false,
   plugins: [
     new LicenseCheckerWebpackPlugin({
-      outputFilename: `app-${app}-notices.txt`
-
-      // TODO: Common license config.
-      // TODO: consider emitError: true
-    }),
-    new LicenseCheckerWebpackPlugin({
+      ...LICENSE_CHECKER_OPTS,
       outputFilename: `app-${app}-notices.json`,
       outputWriter: (data) => {
         // Display complete license list to console.
@@ -65,6 +85,11 @@ module.exports = APPS.map((app) => ({
         // Format output.
         return JSON.stringify(data, null, 2)
       }
+    }),
+    new LicenseCheckerWebpackPlugin({
+      ...LICENSE_CHECKER_OPTS,
+      outputFilename: `app-${app}-notices.txt`,
+      emitError: FAIL_ON_UNAPPROVED
     })
   ]
 }));
